2d243295d90117b9725976dc9eccdb2b5323d17c
Roselle Wang <roselle.wang@berkeley.edu>
2023-12-12 03:08:38 -0800

    results

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # SynBioBench
 Final Project for BioE 134: Synthetic Biology Benchmarking for Large Language Models
+## Table of Contents
+- [SynBioBench](#synbiobench)
+  - [Table of Contents](#table-of-contents)
+  - [How to run](#how-to-run)
+    - [Setting Up Environment](#setting-up-environment)
+    - [Set up your API keys](#set-up-your-api-keys)
+    - [Running the Benchmarking Tests](#running-the-benchmarking-tests)
+    - [Adding Models](#adding-models)
+    - [Adding Tests](#adding-tests)
+  - [Benchmarking Design](#benchmarking-design)
+  - [Results](#results)
+  - [Limitations](#limitations)
+  - [Things to improve](#things-to-improve)
+  - [Author](#author)
+  - [Acknowledgements and Cited Work](#acknowledgements-and-cited-work)
+
+## How to run
+### Setting Up Environment
+1. Clone this repository
+2. Create a virtual environment and activate it
+```
+python -m venv /path/to/new/venv
+source /path/to/new/venv/bin/activate
+```
+3. Install the packages listed in **requirements.txt**
+```
+pip install -r requirements.txt
+```
+**OR**
+1. Make sure you have installed miniconda in your local environment
+2. Create a new conda environment and activate it
+```
+conda env create -f environment.yml
+conda activate synbiobench
+```
+
+### Set up your API keys
+
+
+### Running the Benchmarking Tests
+In the virtual python environment/conda environment, run the following command:
+`python execution.py`
+This automatically runs the benchmarking tests and saves the results by model in csv format under results directory.
+
+
+### Adding Models
+This benchmarking test suite use the langchain chat models interface to call chat models and create sequential chains using the LLMs. First, check if the model is supported by Langchain, follow the instructions and pattermatch the first severals lines in Main in **execution.py**. 
+
+
+### Adding Tests
+If you want to add any new tests, you can add them in the **synbioEval.jsonl** file. Each line in the file represents a test case. Each test case has the following format:
+```
+{"task_id": "SynbioEval/0, this is the task id number",
+ "prompt": "This is the question you want to ask the model", 
+ "solution": "this is the standard soltuion to the question"}
+```
+
+## Benchmarking Design
+I designed 31 questions to test the models' knowledge of the SynBio domain. The questions are divided into 2 categories: Multiple choices questions and free response questions. The Multiple choices questions are designed to test the models' ability to recognize the correct answer to the question. The free response questions are in general more open-ended and designed to test the models' ability to follow the central dogma with the given DNA, RNA sequences.
+
+For every model, I test their ability in both direct inference and using more advanced prompt engineering techniques : [Chain of Thoughts (CoT)](https://arxiv.org/abs/2205.11916)
+
+Due to API availability, I only tested the models that I have access to: GPT 3.5 Turbo, GPT 4, PaLM 2 (Bison model) and Cohere
+
+All of the models are tested under the chat scenerio, to create a easy to use interface, I used [langchain](https://www.langchain.com/) to wrap up the models and create sequencial chains to implement chain of thoughts.
+
+## Results
+| Model | MC Accuracy @ Regular| MC Accuracy @ CoT | FR Accuracy @ Regular | FR Accuracy @ CoT | Regular Accuracy | CoT Accuracy | Total Accuracy |
+| ----- | --------------------- | ------------------ | --------------------- | ------------------ | ----------------- | -------------- | -------------- |
+| GPT 3.5 Turbo | 0.53 | 0.28 | 0.125 | 0.1875 | 0.32| 0.26 | 0.29 |
+| GPT 4 | 0.93 | 0.4 | 0.25 | 0.25| 0.65| 0.32| 0.48|
+| PaLM (Bison)| 0.73| 0.53 |0.125| 0.1875 | 0.42 | 0.35 | 0.26|
+| Cohere | 0.47 | 0.2 | 0.06 | 0.06 | 0.26 | 0.13| 0.13|
+
+
+
+
+## Limitations
+This is a non comprehensive benchmarking test suite of the ability for large language models to perform synthetic biology tasks. The models tested in the original code is also limited as I wasn't able to get access to certain API keys from certain companies.
+
+The test suite is also limited in the number of questions it currently has. 
+
+
+
+## Things to improve
+
+
+## Author
+Roselle Wang - [ruohongrosellew](https://github.com/ruohongrosellew)
+
+
+## Acknowledgements and Cited Work
+Some of the multiple choices questions in the test suite are derived from the Bio 1A and BIOE 10 course at the University of California, Berkeley. 
---
+++ execution.py
@@ -0,0 +1,148 @@
+from typing import Optional, Callable, Dict, Iterable
+import ast
+import contextlib
+import faulthandler
+import io
+import os
+import multiprocessing
+import platform
+import signal
+import tempfile
+import gzip
+import json
+import os
+from langchain.agents import AgentType, Tool, initialize_agent, load_tools
+from langchain.chat_models import ChatOpenAI, ChatCohere
+from getpass import getpass
+from langchain.chat_models import ChatVertexAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
+import google.generativeai as palm
+from langchain.chat_models import ChatGooglePalm
+import pandas as pd
+
+
+def stream_jsonl(filename: str) -> Iterable[Dict]:
+    """
+    Parses each jsonl line and yields it as a dictionary
+    """
+    if filename.endswith(".gz"):
+        with open(filename, "rb") as gzfp:
+            for line in fp:
+                if any(not x.isspace() for x in line):
+                    yield json.loads(line)
+    else:
+        with open(filename, "r") as fp:
+            for line in fp:
+                if any(not x.isspace() for x in line):
+                    yield json.loads(line)
+
+
+def check_correctness_llm(llm, problem: Dict, trial: int, timeout: float,
+                      completion_id: Optional[int] = None) -> Dict:
+    """
+    Evaluates the functional correctness of a completion by running the test
+    suite provided in the problem. 
+
+    :param completion_id: an optional completion ID so we can match
+        the results later even if execution finishes asynchronously.
+    """
+    print(problem["task_id"])
+    try: 
+        result = llm.invoke(str(problem['prompt']))
+    except Exception as e:
+        print(e)
+        return dict(
+        task_id= problem["task_id"],
+        test_type = "regular",
+        passed= "DID NOT COMPLETE",
+        result= "",
+        solution = problem["solution"],
+        completion_id=trial,
+    )
+    parser = StrOutputParser()
+    answer = parser.invoke(result)
+    return dict(
+        task_id= problem["task_id"],
+        test_type = "regular",
+        passed= answer == problem['solution'],
+        result= answer,
+        solution = problem["solution"],
+        completion_id=trial,
+    )
+
+def check_correctness_llm_chain (chain, problem: Dict, trial: int, timeout: float,
+                      completion_id: Optional[int] = None) -> Dict:
+    
+    print(problem["task_id"])
+    try:
+        result = chain.invoke({'question': str(problem['prompt'])})
+    except Exception as e:
+        print(e)
+        return dict(
+        task_id= problem["task_id"],
+        test_type = "regular",
+        passed= "DID NOT COMPLETE",
+        result= "",
+        solution = problem["solution"],
+        completion_id=trial,
+        )
+    return dict(
+        task_id=problem["task_id"],
+        test_type = "cot",
+        passed= result == problem["solution"],
+        result= result,
+        solution = problem["solution"],
+        completion_id= trial,
+    )
+
+def create_cot_chain(llm):
+    thought_prompt = PromptTemplate.from_template(
+        """ You are a synthetic biologist, follow the instructions and answer the questions
+        {question}
+        Let's think step by step """
+    )
+
+    answer_prompt = PromptTemplate.from_template(
+        """
+    {thought}
+    Answer:"""
+    )
+    chain = (
+        {"thought": thought_prompt | llm | StrOutputParser()}
+        | answer_prompt
+        | llm
+        | StrOutputParser()
+    )
+    return chain
+    
+
+if __name__ == "__main__":
+    openai_35_llm = ChatOpenAI(temperature=0, model= "gpt-3.5-turbo-1106", openai_api_key="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")
+    openai_4_llm = ChatOpenAI(temperature=0, model= "gpt-4", openai_api_key="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")
+    cohere_llm = ChatCohere(temperature=0, verbose = True, cohere_api_key="c5UeIX8QBfcGoiL1qo0o4JVyz0bonYoPhfPF9fgD")
+    palm_llm = ChatGooglePalm(temperature=0, model = "chat-bison-001", google_api_key='AIzaSyB5UbSyvLV7lJPtlgyjt6xmWLvpZ_wdqQc')
+    palm_chain = create_cot_chain(palm_llm)
+    openai_35_llm_chain = create_cot_chain(openai_35_llm)
+    openai_4_llm_chain = create_cot_chain(openai_4_llm)
+    cohere_llm_chain = create_cot_chain(cohere_llm)
+    
+    models = {"cohere": [cohere_llm, cohere_llm_chain]}
+
+    #models = {"openai-3.5-turbo": [openai_35_llm, openai_35_llm_chain], "openai-4": [openai_4_llm, openai_4_llm_chain], "cohere": [cohere_llm, cohere_llm_chain], "palm": [palm_llm, palm_chain]}
+    results = {}
+        
+    for model in models:
+        result = []
+        if model not in results:
+            results[model] = pd.DataFrame(columns = ["task_id", "test_type", "passed", "result", "solution", "completion_id"])
+        for i in [0, 1, 2]:
+            suite = stream_jsonl('synbioEval.jsonl')
+            for item in suite:
+                result.append(check_correctness_llm(models[model][0], problem=item, trial= i+1, timeout=10))
+                result.append(check_correctness_llm_chain(models[model][1], problem=item, trial = i+1, timeout=10))
+        for data_dict in result:
+            df_temp = pd.DataFrame([data_dict])
+            results[model] = pd.concat([results[model], df_temp], ignore_index=True)
+        results[model].to_csv(f"results/{model}.csv")
